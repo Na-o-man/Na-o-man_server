9ba42b248a72a432f3b356806ff67c09da604c0b
bflykky <kkyxw@soongsil.ac.kr>
2024-07-25 13:11:14 +0900

    setup: 프로젝트 세팅

---
+++ src/main/java/com/umc/naoman/NaomanApplication.java
@@ -0,0 +1,13 @@
+package com.umc.naoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaomanApplication.class, args);
+	}
+
+}
