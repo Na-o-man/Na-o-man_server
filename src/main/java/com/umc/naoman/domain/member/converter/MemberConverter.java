452b73188eeb669f61f9a90ded3354cf7839b8b5
LEE K.S <kingslee0619@gmail.com>
2024-07-28 17:18:08 +0900

    feat: 특정 회원 정보 조회 api 구현

--- src/main/java/com/umc/naoman/domain/member/converter/MemberConverter.java
+++ src/main/java/com/umc/naoman/domain/member/converter/MemberConverter.java
@@ -1,7 +1,17 @@
 package com.umc.naoman.domain.member.converter;
 
+import com.umc.naoman.domain.member.dto.MemberResponse;
+import com.umc.naoman.domain.member.entity.Member;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MemberConverter {
+
+    public static MemberResponse.MemberInfoByMemberIdDTO toMemberInfoByMemberIdDTO(Member member) {
+        return MemberResponse.MemberInfoByMemberIdDTO.builder()
+                .name(member.getName())
+                .email(member.getEmail())
+                .image(member.getImage())
+                .build();
+    }
 }
---
+++ src/main/java/com/umc/naoman/domain/member/dto/MemberResponse.java
@@ -0,0 +1,20 @@
+package com.umc.naoman.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public abstract class MemberResponse {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MemberInfoByMemberIdDTO extends MemberResponse { //특정 회원 조회
+        String name;
+        String email;
+        String image;
+    }
+
+}
