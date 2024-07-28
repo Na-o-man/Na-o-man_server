440b70536434d2eaf0a661a76649b0c753ad066e
김혜령 <hyeryoung27@soongsil.ac.kr>
2024-07-28 11:19:15 +0900

    [Fix] 어노테이션 추가

--- src/main/java/com/umc/naoman/domain/shareGroup/repository/ProfileRepository.java
+++ src/main/java/com/umc/naoman/domain/shareGroup/repository/ProfileRepository.java
@@ -2,9 +2,11 @@ package com.umc.naoman.domain.shareGroup.repository;
 
 import com.umc.naoman.domain.shareGroup.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     List<Profile> findByShareGroupId(Long shareGroupId);
 }
--- src/main/java/com/umc/naoman/domain/shareGroup/repository/ShareGroupRepository.java
+++ src/main/java/com/umc/naoman/domain/shareGroup/repository/ShareGroupRepository.java
@@ -2,6 +2,8 @@ package com.umc.naoman.domain.shareGroup.repository;
 
 import com.umc.naoman.domain.shareGroup.entity.ShareGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShareGroupRepository extends JpaRepository<ShareGroup, Long> {
 }
