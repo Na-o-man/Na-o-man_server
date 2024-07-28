dfc969b0b698019b834e6bdba9765c0dc63ce6ca
LEE K.S <kingslee0619@gmail.com>
2024-07-28 16:34:43 +0900

    특정회원정보 조회 api 구현

---
+++ src/main/java/com/umc/naoman/domain/member/controller/MemberController.java
@@ -0,0 +1,31 @@
+package com.umc.naoman.domain.member.controller;
+
+
+import com.umc.naoman.domain.member.converter.MemberConverter;
+import com.umc.naoman.domain.member.dto.MemberResponse;
+import com.umc.naoman.domain.member.entity.Member;
+import com.umc.naoman.domain.member.service.MemberService;
+import com.umc.naoman.global.result.ResultResponse;
+import com.umc.naoman.global.result.code.MemberResultCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+@Slf4j
+public class MemberController {
+
+    private MemberService memberService;
+
+    @GetMapping("/info/{memberId}") // memberId를 사용해 특정 회원 정보 조회
+    public ResultResponse<MemberResponse.MemberInfoByMemberIdDTO> getMemberInfoByMemberId(@PathVariable(name = "memberId") Long memberId) {
+        Member member = memberService.findMember(memberId);
+        return ResultResponse.of(MemberResultCode.MEMBER_INFO,
+                MemberConverter.toMemberInfoByMemberIdDTO(member));
+    }
+}
---
+++ src/main/java/com/umc/naoman/domain/member/dto/MemberRequest.java
@@ -0,0 +1,4 @@
+package com.umc.naoman.domain.member.dto;
+
+public abstract class MemberRequest {
+}
