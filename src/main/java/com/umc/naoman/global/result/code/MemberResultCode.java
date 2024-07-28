9898d6503061ff3461f2ab02801a5a1e8e0e0f39
LEE K.S <kingslee0619@gmail.com>
2024-07-28 16:34:29 +0900

    특정회원정보 조회 성공 시 result code 추가

--- src/main/java/com/umc/naoman/global/result/code/MemberResultCode.java
+++ src/main/java/com/umc/naoman/global/result/code/MemberResultCode.java
@@ -13,7 +13,7 @@ public enum MemberResultCode implements ResultCode {
     CHARGE_MY_POINT(200, "SM003","포인트를 성공적으로 충전하였습니다."),
     GET_MY_POINT(200, "SM004", "내 포인트를 성공적으로 조회하였습니다."),
     LOGIN(200, "SM000", "성공적으로 로그인하였습니다."),
-
+    MEMBER_INFO (200,"SM005","회원 정보를 성공적으로 조회하였습니다."),
     ;
     private final int status;
     private final String code;
