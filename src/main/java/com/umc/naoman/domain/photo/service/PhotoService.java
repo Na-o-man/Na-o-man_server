45de0f324b0a0bb496559061c52c9475b70cdec2
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-07-31 02:21:48 +0900

    feat : 특정 공유 그룹의 사진 조회 API 구현

--- src/main/java/com/umc/naoman/domain/photo/service/PhotoService.java
+++ src/main/java/com/umc/naoman/domain/photo/service/PhotoService.java
@@ -2,6 +2,9 @@ package com.umc.naoman.domain.photo.service;
 
 import com.umc.naoman.domain.photo.dto.PhotoRequest;
 import com.umc.naoman.domain.photo.dto.PhotoResponse;
+import com.umc.naoman.domain.photo.entity.Photo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,4 +13,6 @@ public interface PhotoService {
     List<PhotoResponse.PreSignedUrlInfo> getPreSignedUrlList(PhotoRequest.PreSignedUrlRequest request);
 
     PhotoResponse.PhotoUploadInfo uploadPhotoList(PhotoRequest.PhotoUploadRequest request);
+
+    Page<Photo> getAllPhotoListByShareGroup(Long shareGroupId, Pageable pageable);
 }
