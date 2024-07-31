37543e6aa3269609929644ca1dd50c6a049395f1
LEE K.S <kingslee0619@gmail.com>
2024-07-31 18:30:39 +0900

    fix: @Transactional 어노테이션 추가

--- src/main/java/com/umc/naoman/domain/agenda/service/AgendaServiceImpl.java
+++ src/main/java/com/umc/naoman/domain/agenda/service/AgendaServiceImpl.java
@@ -1,7 +1,6 @@
 package com.umc.naoman.domain.agenda.service;
 
 import com.umc.naoman.domain.agenda.converter.AgendaConverter;
-import com.umc.naoman.domain.agenda.dto.AgendaRequest;
 import com.umc.naoman.domain.agenda.entity.Agenda;
 import com.umc.naoman.domain.agenda.repository.AgendaRepository;
 import com.umc.naoman.domain.member.entity.Member;
@@ -12,6 +11,7 @@ import com.umc.naoman.global.error.code.AgendaErrorCode;
 import com.umc.naoman.global.error.code.ShareGroupErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,12 +21,14 @@ public class AgendaServiceImpl implements AgendaService {
     private final ShareGroupRepository shareGroupRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Agenda findAgenda(Long agendaId) {
         return agendaRepository.findById(agendaId)
                 .orElseThrow(() -> new BusinessException(AgendaErrorCode.AGENDA_NOT_FOUND_BY_AGENDA_ID));
     }
 
     @Override
+    @Transactional
     public Agenda createAgenda(Member member, Long shareGroupId, String title) {
         ShareGroup shareGroup = shareGroupRepository.findById(shareGroupId)
                 .orElseThrow(() -> new BusinessException(ShareGroupErrorCode.SHARE_GROUP_NOT_FOUND));
