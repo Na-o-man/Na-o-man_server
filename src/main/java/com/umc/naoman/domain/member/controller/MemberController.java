d8eb950d04866a6170cc9bdea830ce1a098a6b31
LEE K.S <kingslee0619@gmail.com>
2024-07-31 13:02:26 +0900

    feat: swagger 추가

--- src/main/java/com/umc/naoman/domain/member/controller/MemberController.java
+++ src/main/java/com/umc/naoman/domain/member/controller/MemberController.java
@@ -4,8 +4,14 @@ import com.umc.naoman.domain.member.converter.MemberConverter;
 import com.umc.naoman.domain.member.dto.MemberResponse;
 import com.umc.naoman.domain.member.entity.Member;
 import com.umc.naoman.domain.member.service.MemberService;
+import com.umc.naoman.global.error.ErrorResponse;
 import com.umc.naoman.global.result.ResultResponse;
 import com.umc.naoman.global.result.code.MemberResultCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +26,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     private final MemberService memberService;
 
+
     @GetMapping("/{memberId}") // memberId를 사용해 특정 회원 정보 조회
+    @Operation( summary = "특정 회원 정보 조회 API", description = "PathVariable]\n memberId\nrequest]\n" +
+            "response]\n uesrname, email, 소셜 프로필 이미지 url")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse
+                    (responseCode = "SM005", description = "특정 회원 정보 조회 성공."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse
+                    (responseCode = "EM001", description = "해당 memberId를 가진 회원이 존재하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
     public ResultResponse<MemberResponse.MemberInfo> getMemberInfo(@PathVariable(name = "memberId") Long memberId) {
         Member member = memberService.findMember(memberId);
         return ResultResponse.of(MemberResultCode.MEMBER_INFO,
@@ -28,6 +44,15 @@ public class MemberController {
     }
 
     @GetMapping("/terms/{memberId}")
+    @Operation(summary = "마케팅 약관 동의 여부 조회 API", description = "PathVariable]\n memberId\nrequest]\n" +
+            "response]\n 마케팅 동의 여부 -> 동의 => true, 비동의 => false")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse
+                    (responseCode = "SM006", description = "마케팅 약관 동의 여부 조회 성공."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse
+                    (responseCode = "EM001", description = "해당 memberId를 가진 회원이 존재하지 않습니다.",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
     public ResultResponse<MemberResponse.MarketingAgreed> getMarketingAgreed(@PathVariable(name = "memberId") Long memberId) {
         Member member = memberService.findMember(memberId);
         return ResultResponse.of(MemberResultCode.CHECK_MARKETING_AGREED, MemberConverter.toMarketingAgreed(member));
