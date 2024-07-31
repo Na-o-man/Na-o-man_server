3ed2fb4a8010cb3f4f8bbf16cba96b46dd1ae9b8
LEE K.S <kingslee0619@gmail.com>
2024-07-31 18:01:07 +0900

    fix: Agenda와 Member 연관관계 설정

--- src/main/java/com/umc/naoman/domain/agenda/entity/Agenda.java
+++ src/main/java/com/umc/naoman/domain/agenda/entity/Agenda.java
@@ -1,16 +1,9 @@
 package com.umc.naoman.domain.agenda.entity;
 
+import com.umc.naoman.domain.member.entity.Member;
 import com.umc.naoman.domain.shareGroup.entity.ShareGroup;
 import com.umc.naoman.global.entity.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,4 +28,7 @@ public class Agenda extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "share_group_id")
     private ShareGroup shareGroup;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
