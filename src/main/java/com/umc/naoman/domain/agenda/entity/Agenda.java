cf31cec3e519946334a690483158c5b69cbd5774
LEE K.S <kingslee0619@gmail.com>
2024-07-31 18:01:07 +0900

    rebase

--- src/main/java/com/umc/naoman/domain/agenda/entity/Agenda.java
+++ src/main/java/com/umc/naoman/domain/agenda/entity/Agenda.java
@@ -3,8 +3,20 @@ package com.umc.naoman.domain.agenda.entity;
 import com.umc.naoman.domain.shareGroup.entity.Profile;
 import com.umc.naoman.domain.shareGroup.entity.ShareGroup;
 import com.umc.naoman.global.entity.BaseTimeEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
