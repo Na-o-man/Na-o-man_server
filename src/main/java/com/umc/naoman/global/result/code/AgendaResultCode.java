5e059c86b60607776fe215bad76ebcda4228b485
LEE K.S <kingslee0619@gmail.com>
2024-07-31 18:13:55 +0900

    feat: 새 안건 추가 성공 resultCode 생성

---
+++ src/main/java/com/umc/naoman/global/result/code/AgendaResultCode.java
@@ -0,0 +1,15 @@
+package com.umc.naoman.global.result.code;
+
+import com.umc.naoman.global.result.ResultCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AgendaResultCode implements ResultCode {
+    CREATE_AGENDA(200, "SA001", "새로운 안건을 성공적으로 생성하였습니다."),
+    ;
+    private final int status;
+    private final String code;
+    private final String message;
+}
