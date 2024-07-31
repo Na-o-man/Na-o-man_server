8916d45309d11ba7518c13233baa08fbcd5e81fb
redblackblossom <physicsdev1029@gmail.com>
2024-07-31 18:46:44 +0900

    feat : Elasticsearch 의존성과 configuration 추가

---
+++ src/main/java/com/umc/naoman/global/config/ElasticsearchConfig.java
@@ -0,0 +1,15 @@
+package com.umc.naoman.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+    }
+}
