031bd9fc50d7233f5161c04c33bf3dd70544200b
redblackblossom <physicsdev1029@gmail.com>
2024-08-05 21:31:23 +0900

    feat: 알림 컨트롤러에 스웨거 추가

--- src/main/java/com/umc/naoman/domain/notification/controller/NotificationController.java
+++ src/main/java/com/umc/naoman/domain/notification/controller/NotificationController.java
@@ -9,8 +9,12 @@ import com.umc.naoman.domain.notification.service.NotificationService;
 import com.umc.naoman.global.result.ResultResponse;
 import com.umc.naoman.global.result.code.NotificationResultCode;
 import com.umc.naoman.global.security.annotation.LoginMember;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,10 +28,20 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/notifications")
+@Tag(name = "알림 관련 API", description = " 알림 전체 목록 조희, fcm 토큰 업로드, 알림 1개 삭제, 알림 전체 삭제, 읽지 않은 알림 유무,  내 알람 전체 읽음 처리를 하는 API입니다.")
 public class NotificationController {
     private final NotificationService notificationService;
 
     @PostMapping("/token")
+    @Operation(
+            summary = "fcm 토큰 업로드",
+            description = "안드로이드가 fcm 토큰을 서버에 업로드하는 API입니다.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            schema = @Schema(implementation = NotificationRequest.FcmToken.class)
+                    )
+            )
+    )
     public ResultResponse<Void> registerFcmToken(@RequestBody NotificationRequest.FcmToken fcmToken,
                                                  @LoginMember Member member){
 
@@ -39,6 +53,7 @@ public class NotificationController {
             @Parameter(name = "page", description = "조회할 페이지를 입력해 주세요.(0번부터 시작)"),
             @Parameter(name = "size", description = "한 페이지에 나타낼 알림 개수를 입력해주세요.")
     })
+    @Operation(summary = "자신의 모든 알림 조회", description = "자신의 모든 알림을 조회하는 API입니다.")
     public ResultResponse<NotificationResponse.PagedNotificationInfo> getNotifications(@LoginMember Member member,
                                                                                        @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC)
                                                                                        @Parameter(hidden = true) Pageable pageable){
@@ -48,6 +63,9 @@ public class NotificationController {
     }
 
     @GetMapping("/unread")
+    @Operation(summary = "읽지 않은 알림 유무 조회",
+            description = "읽지 않은 알림 유무 조회하는 API입니다.",
+            parameters = {})
     public ResultResponse<NotificationResponse.UnreadNotification> getIsUnread(@LoginMember Member member){
         List<Notification> notificationList =  notificationService.isUnreadNotification(member);
         return ResultResponse.of(NotificationResultCode.CHECK_MY_UNREAD_NOTIFICATION,
@@ -55,6 +73,9 @@ public class NotificationController {
     }
 
     @PostMapping("/acknowledgements")
+    @Operation(summary = "모든 알림 읽음 처리",
+            description = "자신의 모든 알림을 읽음 처리하는 API입니다.",
+            parameters = {})
     public ResultResponse<NotificationResponse.NotificationAcknowledgeCount> setMyNotificationRead(@LoginMember Member member){
         List<Notification> notificationList = notificationService.setMyNotificationRead(member);
         return ResultResponse.of(NotificationResultCode.READ_ALL_MY_NOTIFICATION,
@@ -62,6 +83,11 @@ public class NotificationController {
     }
 
     @DeleteMapping("/{notificationId}")
+    @Operation(summary = "특정 알림 1개 삭제",
+            description = "특정 알림 1개를 삭제하는 API입니다.",
+            parameters = {
+            @Parameter(name = "notificationId", description = "삭제할 알림의 Id", required = true, schema = @Schema(type = "long"))
+    })
     public ResultResponse<NotificationResponse.NotificationAcknowledgeCount> deleteNotification(@PathVariable Long notificationId,
                                                                                                 @LoginMember Member member){
         long deletedCount = notificationService.deleteNotification(member,notificationId);
@@ -69,6 +95,9 @@ public class NotificationController {
                 NotificationConverter.toNotificationAcknowledgedCount(deletedCount));
     }
     @DeleteMapping
+    @Operation(summary = "모든 알림 삭제",
+            description = "자신의 모든 알림을 삭제하는 API입니다.",
+            parameters = {})
     public ResultResponse<NotificationResponse.NotificationDeletedCount> deleteAllNotification(@LoginMember Member member){
         long deletedCount = notificationService.deleteNotificationAll(member);
         return ResultResponse.of(NotificationResultCode.DELETE_MY_NOTIFICATION,
