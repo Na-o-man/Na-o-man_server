4f9804c22d31afd2e45d771ba65a5c5cf25f7d16
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-08-11 14:19:43 +0900

    fix : 삭제 로직 벌크 삭제로 수정

--- src/main/java/com/umc/naoman/domain/photo/repository/PhotoRepository.java
+++ src/main/java/com/umc/naoman/domain/photo/repository/PhotoRepository.java
@@ -2,6 +2,9 @@ package com.umc.naoman.domain.photo.repository;
 
 import com.umc.naoman.domain.photo.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,7 @@ import java.util.List;
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
     List<Photo> findByIdInAndShareGroupId(List<Long> photoIdList, Long shareGroupId);
     List<Photo> findByIdIn(List<Long> photoIdList);
+    @Modifying
+    @Query("DELETE FROM Photo p WHERE p.id IN :idList")
+    void deleteAllByIdList(@Param("idList") List<Long> idList);
 }
